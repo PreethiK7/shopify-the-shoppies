e53e92ae0013fb599dbe291d4c8e3e372f878df5
Preethi Kesavan <preethikesavan7@gmail.com>
2020-08-29 22:59:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client/build'));
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
+app.use(express.static(__dirname + '/'));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
